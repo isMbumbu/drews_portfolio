21e9b198bc395456f63e6fa9e7fb367c99064d75
Andrew Ambuka <andrewambuka9@gmail.com>
2025-06-20 08:28:03 +0300

    Update HomeC.jsx

--- src/components/HomeC.jsx
+++ src/components/HomeC.jsx
@@ -21,26 +21,26 @@ function HomeC() {
     return () => clearInterval(interval);
   }, []);
 
-  useEffect(() => {
-    const createSnowflake = () => {
-      const snowflake = document.createElement('div');
-      snowflake.className = 'snowflake';
-      snowflake.innerText = '❆';
-      snowflake.style.left = `${Math.random() * 100}vw`;
-      snowflake.style.animationDuration = `${3 + Math.random() * 5}s`;
-      snowflake.style.opacity = Math.random();
-      snowflake.style.fontSize = `${Math.random() * 1.5 + 1}rem`;
-      snowflake.style.transform = `rotate(${Math.random() * 360}deg)`;
-      document.body.appendChild(snowflake);
+  // useEffect(() => {
+  //   const createSnowflake = () => {
+  //     const snowflake = document.createElement('div');
+  //     snowflake.className = 'snowflake';
+  //     snowflake.innerText = '❆';
+  //     snowflake.style.left = `${Math.random() * 100}vw`;
+  //     snowflake.style.animationDuration = `${3 + Math.random() * 5}s`;
+  //     snowflake.style.opacity = Math.random();
+  //     snowflake.style.fontSize = `${Math.random() * 1.5 + 1}rem`;
+  //     snowflake.style.transform = `rotate(${Math.random() * 360}deg)`;
+  //     document.body.appendChild(snowflake);
 
-      setTimeout(() => {
-        snowflake.remove();
-      }, 8000);
-    };
+  //     setTimeout(() => {
+  //       snowflake.remove();
+  //     }, 8000);
+  //   };
 
-    const snowInterval = setInterval(createSnowflake, 200);
-    return () => clearInterval(snowInterval);
-  }, []);
+  //   const snowInterval = setInterval(createSnowflake, 200);
+  //   return () => clearInterval(snowInterval);
+  // }, []);
 
   return (
     <div className='home container-fluid container-sm fade-slide'>
