41147cb67a0045e366dcc7d331083b044b8af46d
isMbumbu <andrewambuka9@gmail.com>
2025-08-12 09:07:05 +0300

    Feature: Add Battleship Game to Portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -32,6 +32,17 @@ function Portfolio() {
           <h3>Agrimmerce</h3>
           <p>A Software as a Solution for local farmers to be able to post their available products online</p>
         </div>
+        <div className="portfolio-item">
+          <a href="https://github.com/CorneliusKibetDC/Mini-Techs-Battleship-Group7" target="_blank" rel="noopener noreferrer">
+            <img 
+              src="battle.png" 
+              alt="BattleShip CLI preview"
+              className="portfolio-image"
+            />
+          </a>
+          <h3>Battleship Game</h3>
+          <p>A Software game which is played on the Command Line Interface (CLI) and provides a simple yet engaging gameplay experience.</p>
+        </div>
         
           
 {/*         <div className="portfolio-item">
