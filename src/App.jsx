6f36e190c0419f297127af013757f652ca842c59
Andrew Ambuka <andrewambuka9@gmail.com>
2026-05-13 18:44:37 +0300

    Update last style

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { motion, useSpring, useMotionValue } from 'framer-motion'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
 import About from './pages/About'
@@ -12,21 +13,46 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+
+  // Smooth spring physics for that "dragging" feel
+  const springConfig = { damping: 20, stiffness: 200, mass: 0.5 };
+  const cursorX = useSpring(mouseX, springConfig);
+  const cursorY = useSpring(mouseY, springConfig);
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      mouseX.set(e.clientX);
+      mouseY.set(e.clientY);
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
 
   return (
-     <BrowserRouter>
-        <Routes>
-          <Route path= '/' element={<Home/>}/>
-          <Route path='/blog' element={<Blog/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-          <Route path='/portfolio' element={<Portfolio/>}/>
-          <Route path='/services' element={<Services/>}/>
-          <Route path='/post' element={<Post/>} />
-  
-        </Routes>
-     </BrowserRouter>
-     
+    <BrowserRouter>
+      {/* Custom Trailing Cursor */}
+      <motion.div 
+        className="custom-cursor"
+        style={{
+          x: cursorX,
+          y: cursorY,
+          translateX: "-50%",
+          translateY: "-50%"
+        }}
+      />
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/blog' element={<Blog/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/portfolio' element={<Portfolio/>}/>
+        <Route path='/services' element={<Services/>}/>
+        <Route path='/post' element={<Post/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
